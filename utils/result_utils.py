0c44d72245a9f4f109410c32af7c20313838b31a
m-bilgic <mbilgic@iit.edu>
2014-12-19 16:47:56 -0600

    utils package.

--- utils/result_utils.py
+++
@@ -1,5 +0,0 @@
-"""
-The :mod:`utils.result_utils` implements the necessary tools for
-manipulating (writing to a file, plotting, etc) the learning curve 
-results.
-"""
---
+++ utils/utils.py
@@ -0,0 +1,3 @@
+"""
+The :mod:`utils.utils` implements various helper functions.
+"""
